751b4d5637b34edcea950d20e48dca2c7765c013
marwadidolly <marwadidolly20@gmail.com>
2025-05-22 01:30:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".hero").style.opacity = "1";
-});
+function filterImages(category) {
+  const images = document.querySelectorAll(".gallery-image");
+  images.forEach(img => {
+    if (category === "all") {
+      img.style.display = "inline-block";
+    } else {
+      img.style.display = img.classList.contains(category) ? "inline-block" : "none";
+    }
+  });
+}
+
